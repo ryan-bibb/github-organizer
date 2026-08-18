9f66ef35007614c8bfd5fb92454708c323fe4dfb
ryan-bibb <ryanbibb34@gmail.com>
2026-08-18 17:44:28 -0400

    wip: working on nav-bar

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,24 +1,15 @@
 import { auth, signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
 
+// TODO: to fix the ui maybe put sign out on right and img and name on right, color the background and make the name lighter
 export async function NavBar() {
   const session = await auth();
 
   return (
-    <div className="flex items-center justify-between px-6 py-4">
-      <div className="flex items-center gap-2">
-        {session?.user?.image && (
-          <Image
-            src={session.user.image}
-            alt={session.user.name ?? "User avatar"}
-            className="size-8 rounded-full"
-            width={32}
-            height={32}
-          />
-        )}
-        <span className="text-sm font-m">{session?.user?.name}</span>
-      </div>
+    <div className="flex items-center justify-end gap-2 px-6 py-4">
+      <span className="text-sm font-medium">{session?.user?.name}</span>
       <form
         action={async () => {
           "use server";
@@ -29,6 +20,16 @@ export async function NavBar() {
           Sign out
         </Button>
       </form>
+      <Badge variant="outline">{session?.user?.name}</Badge>
+      {session?.user?.image && (
+        <Image
+          src={session.user.image}
+          alt={session.user.name ?? "User avatar"}
+          className="size-8 rounded-full"
+          width={32}
+          height={32}
+        />
+      )}
     </div>
   );
 }
