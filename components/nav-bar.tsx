c3eacc17d9f5cbed03f94512e94da1c9b185f8c2
ryan-bibb <ryanbibb34@gmail.com>
2026-08-19 15:23:24 -0400

    refactor(ui): add justify between nav-bar and main content

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -17,7 +17,7 @@ export async function NavBar() {
   const session = await auth();
 
   return (
-    <div className="flex items-center justify-between gap-2 px-6 py-4">
+    <div className="flex items-center justify-between gap-2 border-b-2 border-primary px-6 py-4">
       <DropdownMenu className="w-full">
         <DropdownMenuTrigger
           render={
